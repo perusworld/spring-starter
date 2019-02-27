0fc01dd533000193c7fa5c96d83955c0d51d26d1
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2019-02-26 17:52:56 -0800

    cleanup local directory

--- sample-sftp-service/src/test/java/com/yosanai/spring/starter/samplesftpservice/TestConfig.java
+++ sample-sftp-service/src/test/java/com/yosanai/spring/starter/samplesftpservice/TestConfig.java
@@ -1,5 +1,8 @@
 package com.yosanai.spring.starter.samplesftpservice;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
@@ -17,8 +20,14 @@ public class TestConfig {
 	@Value("${samplesftp.remote.directory}")
 	private String initDir;
 
+	@Value("${samplesftp.local.directory}")
+	private String localDirectory;
+
 	@Bean
-	public TestSftpServer sftpServer(@Autowired DefaultSftpSessionFactory session) {
+	public TestSftpServer sftpServer(@Autowired DefaultSftpSessionFactory session) throws Exception {
+		if (localDirectory.startsWith(FileUtils.getTempDirectoryPath())) {
+			FileUtils.deleteDirectory(new File(localDirectory));
+		}
 		int port = SocketUtils.findAvailableTcpPort(2500);
 		session.setPort(port);
 		TestSftpServer ret = new TestSftpServer(port, initDir);
